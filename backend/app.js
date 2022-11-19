23faf5b09325aff1b3a1acaf2368a4bb78e4426e
Louie Weng <louieweng@outlook.com>
2022-11-19 14:54:25 -0800

    added mock data endpoint for testing (#5)
    Co-authored-by: Louie Weng <louieweng@dhcp-206-87-129-41.ubcsecure.wireless.ubc.ca>

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import cors from 'cors'
+
+import { readFileSync } from "fs";
+const fileUrl = new URL("./test-data/groups.json", import.meta.url);
+const groupsData = JSON.parse(readFileSync(fileUrl));
 
 const app = express()
+app.use(cors())
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -9,4 +15,8 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/test', (req, res) => {
+    res.send(groupsData)
 })
--- my-app/src/routes/create/+page.svelte
+++ my-app/src/routes/create/+page.svelte
@@ -1,3 +1,2 @@
-
-<h1> this is where you create a group</h1>
+<h1>this is where you create a group</h1>
 <a href="/send">Send message</a>

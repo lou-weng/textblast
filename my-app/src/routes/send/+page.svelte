0b6475694294b6b23880ecf1cf9b8be176abfcf9
m20arcusk <marcuskam20@gmail.com>
2022-11-19 14:59:11 -0800

    Setup Components for homepage and create page

--- my-app/src/routes/send/+page.svelte
+++ my-app/src/routes/send/+page.svelte
@@ -1,2 +1,12 @@
 <h1>this is where you send messages</h1>
 <a href="/create">Create a group</a>
+<<<<<<< HEAD
+=======
+<!--
+<script>
+    let sampleJson = {
+        ""
+    }
+</script>
+-->
+>>>>>>> 80fafd8 (Setup Components for homepage and create page)
